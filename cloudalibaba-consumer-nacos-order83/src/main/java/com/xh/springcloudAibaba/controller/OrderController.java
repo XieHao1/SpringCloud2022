8a3f708f595983a2df6b9e4a54b4e294754a935d
谢豪 <1693062665@qq.com>
2022-05-22 22:08:36 +0800

    springCloudAibaba学习day1

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/xh/springcloudAibaba/controller/OrderController.java
@@ -0,0 +1,24 @@
+package com.xh.springcloudAibaba.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderController {
+
+    private static final String URL_PREFIX = "http://nacos-payment-provider";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping(value = "/consumer/payment/nacos/{id}")
+    public String paymentInfo(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(URL_PREFIX+"/payment/nacos/"+id,String.class);
+    }
+}
